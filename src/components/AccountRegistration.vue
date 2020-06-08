af9d105cee60e48f79f12b5a251de12e0a910c42
AlejandraDay <alejandra.day.123@gmail.com>
2020-06-07 21:35:58 -0400

    Add component AccountRegistration

---
+++ src/components/AccountRegistration.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <br />
+    <button @click="AddNewAccount()">Register Account</button>
+  </div>
+</template>
+<script>
+import account from "./account.json";
+export default {
+  name: "AccountRegistration",
+  props: {
+    name: {
+      type: String,
+      default: ""
+    },
+    cd: {
+      type: String,
+      default: ""
+    },
+    email: {
+      type: String,
+      default: ""
+    },
+    pwd: {
+      type: String,
+      default: ""
+    },
+    verifyPwd: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    AddNewAccount() {
+      var obj = account;
+      JSON.stringify(obj.push({ b: 2 }));
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!--img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /-->
+    <label>Name: </label>
+    <input type="text" v-model="InputName" />
+    <br />
+    <br />
+    <label>CI: </label>
+    <input type="text" v-model="InputCI" />
+    <br />
+    <br />
+    <label> Email: </label>
+    <input type="email" v-model="InputEmail" />
+    <br />
+    <br />
+    <label>Password: </label>
+    <input type="password" v-model="InputPwd" />
+    <br />
+    <br />
+    <label>Confirm Password: </label>
+    <input type="password" v-model="InputVerifyPwd" />
+    <br />
+
+    <AccountRegistration
+      :name="InputName"
+      :cd="InputCI"
+      :email="InputEmail"
+      :pwd="InputPwd"
+      :verifyPwd="InputVerifyPwd"
+    ></AccountRegistration>
+    <br />
+    <br />
+    <br />
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+//import HelloWorld from "@/components/HelloWorld.vue";
+import AccountRegistration from "@/components/AccountRegistration.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    //HelloWorld
+    AccountRegistration
+  },
+  data() {
+    return {
+      InputName: "",
+      InputCI: "",
+      InputEmail: "",
+      InputPwd: "",
+      InputVerifyPwd: ""
+    };
   }
 };
 </script>
