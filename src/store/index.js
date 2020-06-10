b325a318fd07832e7b89023af7430effe8b393f6
Nicolas Carrasco <nico.carrasco.arandia@outlook.com>
2020-06-09 21:04:13 -0400

    expenses: added initial data

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,44 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    USUARIOS: [
+      { ci: 0, nombre: "general", email: "general@ejemplo.com", pwd: "admin" }
+    ],
+    CABECERAS: [["ID", "Categoria", "Monto", "Descripción"]],
+    MOVIMIENTOS: [
+      {
+        usuario: 0,
+        id: 101,
+        categoria: "I0",
+        monto: 500,
+        descripcion: "descripción 1"
+      },
+      {
+        usuario: 0,
+        id: 111,
+        categoria: "E0",
+        monto: 50,
+        descripcion: "descripción 2"
+      }
+    ],
+    CUENTA_ACTUAL: -1,
+    CATEGORIAS: [
+      { id: "I0", nombre: "otros ingresos", usuario: 0 },
+      { id: "E0", nombre: "otros egresos", usuario: 0 }
+    ]
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+  plugins: [
+    new VuexPersistence({
+      key: "gestor",
+      storage: window.localStorage
+    }).plugin
+  ]
+});
