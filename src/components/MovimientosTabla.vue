a11417067c1a4e766de5b8e88a85d1cc9d458c57
Nicolas Carrasco <nico.carrasco.arandia@outlook.com>
2020-06-09 21:05:01 -0400

    expenses: basic view and component created

---
+++ src/components/MovimientosTabla.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="tabla">
+    <h1>{{ msg }}</h1>
+    <table>
+      <thead>
+        <tr>
+          <th :key="columna" v-for="columna in this.cabeceras">
+            {{ columna }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="item.id" v-for="item in movimientos">
+          <td>{{ item.id }}</td>
+          <td>{{ item.categoria }}</td>
+          <td>{{ item.monto }}</td>
+          <td>{{ item.descripcion }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovimientosTabla",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      filtro: "",
+      categoria: "todas"
+    };
+  },
+  computed: {
+    movimientos() {
+      return this.$store.state.MOVIMIENTOS;
+    },
+    cabeceras() {
+      return this.$store.state.CABECERAS[0];
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tabla {
+  font-size: 18px;
+  color: brown;
+}
+
+table,
+th,
+td {
+  border: 1px solid black;
+}
+
+table {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+
+td {
+  color: black;
+}
+</style>
---
+++ src/views/Movimientos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MovimientosTabla msg="Esta es la sección de la tabla de movimientos" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MovimientosTabla from "@/components/MovimientosTabla.vue";
+
+export default {
+  name: "Movimientos",
+  components: {
+    MovimientosTabla
+  }
+};
+</script>
+
+<style scoped>
+div {
+  font-family: Helvetica, sans-serif;
+}
+</style>
