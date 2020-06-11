45144e45e4d4d16e4cb0cb6bdbf2912a33cf83ff
juandigv69 <juandigv69@gmail.com>
2020-06-11 14:52:42 -0400

    added Categories to router / store index

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    USERS:[
+      {ci: 0, name: 'admin', email: 'admin@default.com',pwd:'admin'}
+    ],
+    CATEGORIES: [
+      {id: 'I0', name: 'others', user: 0},
+      {id: 'E0', name: 'others', user: 0}
+    ]
   },
   mutations: {
   },
